622e2ae18445e51c3a16840d9d28205231402d88
Moleen-c-lame <abc25-006@thuto.bac.ac.bw>
2026-04-18 16:47:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function showMessage() {
     alert("💖 Thank you for shopping with Hotties.Brandz!");
 }
+window.addEventListener("scroll", function () {
+    let elements = document.querySelectorAll(".reveal");
+
+    elements.forEach(el => {
+        let position = el.getBoundingClientRect().top;
+        let screenHeight = window.innerHeight;
+
+        if (position < screenHeight - 100) {
+            el.classList.add("active");
+        }
+    });
+});
