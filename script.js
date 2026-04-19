3eabd3c77eb195226d29d2f38e914892765bf266
Moleen-c-lame <abc25-006@thuto.bac.ac.bw>
2026-04-19 10:55:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ window.addEventListener("scroll", function () {
         }
     });
 });
+window.addEventListener("scroll", () => {
+    document.querySelectorAll(".reveal").forEach(el => {
+        if (el.getBoundingClientRect().top < window.innerHeight - 100) {
+            el.classList.add("active");
+        }
+    });
+});
