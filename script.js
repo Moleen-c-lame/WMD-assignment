a1c016c9ed3074cadaf343e6bdbf00a7ea792b34
Moleen-c-lame <abc25-006@thuto.bac.ac.bw>
2026-04-20 17:29:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -66,5 +66,4 @@ function clearCart() {
     document.getElementById("cart-items").innerHTML = "";
     total = 0;
     document.getElementById("total").textContent = total;
-    if(confirm("Clear all items?")) { ... }
 }
